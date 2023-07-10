8e3ad3fd02adcab3eb19eb6587fc9ac178decb97
xyluo <36498464+Xiangyongluo@users.noreply.github.com>
2023-07-09 20:52:00 -0700

    init update

---
+++ pyutilkit/officeutil/pdf.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+##############################################################
+# Created Date: Sunday, July 9th 2023
+# Contact Info: luoxiangyong01@gmail.com
+# Author/Copyright: Mr. Xiangyong Luo
+##############################################################
+
+
+# pdf to word
+# pdf to text
+
+# pdf operation
---
+++ pyutilkit/officeutil/word.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+##############################################################
+# Created Date: Sunday, July 9th 2023
+# Contact Info: luoxiangyong01@gmail.com
+# Author/Copyright: Mr. Xiangyong Luo
+##############################################################
+
+
+# word to pdf
+# word to text
