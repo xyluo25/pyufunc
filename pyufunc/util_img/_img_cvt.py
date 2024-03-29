3cca7e96ee79e1395a8475099bbab54072407cd4
xyluo <36498464+Xiangyongluo@users.noreply.github.com>
2024-03-29 12:38:35 -0700

    add img to bytes and viceverse fucntions

---
+++ pyufunc/util_img/_img_cvt.py
@@ -0,0 +1,81 @@
+# -*- coding:utf-8 -*-
+##############################################################
+# Created Date: Friday, March 29th 2024
+# Contact Info: luoxiangyong01@gmail.com
+# Author/Copyright: Mr. Xiangyong Luo
+##############################################################
+
+
+from PIL import Image
+import io
+
+
+def cvt_img_to_bytes(img_path: str) -> bytes:
+    """Convert image to bytes
+
+    Args:
+        img_path (str): image path, include all image format, e.g. .jpg, .png, .bmp, .gif, etc.
+
+    Returns:
+        bytes: the image bytes
+
+    Example:
+        >>> from pyufunc import cvt_img_to_bytes
+        >>> img_path = 'test.jpg'
+        >>> img_b = cvt_img_to_bytes(img_path)
+        >>> print(img_b)
+    """
+    try:
+        with open(img_path, 'rb') as f:
+            img_b = f.read()
+    except Exception:
+        img_b = bytes("", encoding='utf-8')
+    return img_b
+
+
+def cvt_PIL_img_to_bytes(img: Image) -> bytes:
+    """Convert PIL image to bytes
+
+    Args:
+        img (Image): PIL image object
+
+    Returns:
+        bytes: the image bytes
+
+    Example:
+        >>> from PIL import Image
+        >>> from pyufunc import cvt_PIL_img_to_bytes
+        >>> img = Image.open('test.jpg')
+        >>> img_b = cvt_PIL_img_to_bytes(img)
+        >>> print(img_b)
+    """
+    try:
+        img_b = io.BytesIO()
+        img.save(img_b, format='JPEG')
+        return img_b.getvalue()
+    except Exception:
+        return bytes("", encoding='utf-8')
+
+
+def cvt_img_bytes_to_PIL_img(img_b: bytes) -> Image:
+    """Convert image bytes to PIL image
+
+    Args:
+        img_b (bytes): image bytes
+
+    Returns:
+        Image: PIL image object
+
+    Example:
+        >>> from PIL import Image
+        >>> from pyufunc import cvt_img_bytes_to_PIL_img
+        >>> img_path = 'test.jpg'
+        >>> img_b = cvt_img_to_bytes(img_path)
+        >>> img = cvt_img_bytes_to_PIL_img(img_b)
+        >>> img.show()
+    """
+    try:
+        img = Image.open(io.BytesIO(img_b))
+        return img
+    except Exception:
+        return Image.new('RGB', (1, 1), (255, 255, 255))
