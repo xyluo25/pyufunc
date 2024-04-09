b3dd6f9a113403b9826677510c57a718686ade02
xyluo25 <luoxiangyong01@gmal.com>
2024-04-09 15:20:47 -0700

    update sort algorithm

--- pyufunc/util_algorithm/_sort.py
+++ pyufunc/util_algorithm/_sort.py
@@ -148,12 +148,105 @@ def bubble_sort(array: Iterable, verbose: bool = False) -> Iterable:
     return array
 
 
-def insertion_sort():
-    pass
+def insertion_sort(array: Iterable, verbose: bool = False) -> Iterable:
+    """Sort the input array using insertion sort algorithm.
+
+    Args:
+        array (Iterable): iterable object to be sorted.
+        verbose (bool, optional): whether to print out running time. Defaults to False.
+
+    Raises:
+        ValueError: Input should be iterable.
+
+    Returns:
+        Iterable: sorted array
+
+    See Also:
+        - https://www.geeksforgeeks.org/insertion-sort/
+
+    Example:
+        >>> from pyufunc import insertion_sort
+        >>> insertion_sort([3, 6, 8, 10, 1, 2, 1])
+        [1, 1, 2, 3, 6, 8, 10]
+        >>> insertion_sort([3, 6, 8, 10, 1, 2, 1], verbose=True)
+        Running time of insertion_sort: O(n^2)
+        [1, 1, 2, 3, 6, 8, 10]
+    """
+
+    # TDD: Test-Driven Development
+    # check if the input is iterable or not
+    if not isinstance(array, Iterable):
+        raise ValueError("Input should be iterable")
+
+    for i in range(1, len(array)):
+        key_item = array[i]
+
+        j = i - 1
+        while j >= 0 and key_item < array[j]:
+            array[j + 1] = array[j]
+            j -= 1
+        array[j + 1] = key_item
+
+    if verbose:
+        print("Running time of insertion_sort: O(n^2)")
+
+    return array
 
 
-def merge_sort():
-    pass
+def merge(left: Iterable, right: Iterable) -> Iterable:
+
+    merged_list = []
+    left_index = 0
+    right_index = 0
+
+    # Merge the two lists together
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] < right[right_index]:
+            merged_list.append(left[left_index])
+            left_index += 1
+        else:
+            merged_list.append(right[right_index])
+            right_index += 1
+
+    # Add the remaining elements in the left list
+    while left_index < len(left):
+        merged_list.append(left[left_index])
+        left_index += 1
+
+    # Add the remaining elements in the right list
+    while right_index < len(right):
+        merged_list.append(right[right_index])
+        right_index += 1
+
+    return merged_list
+
+
+def merge_sort(array: Iterable, verbose: bool = False) -> Iterable:
+
+    # TDD: Test-Driven Development
+    # check if the input is iterable or not
+    if not isinstance(array, Iterable):
+        raise ValueError("Input should be iterable")
+
+    # check if the input array is larger than 1
+    if len(array) <= 1:
+        return array
+
+    # split the array into two parts
+    mid = len(array) // 2
+    left = array[:mid]
+    right = array[mid:]
+
+    # recursively sort the left and right parts
+    left = merge_sort(left)
+    right = merge_sort(right)
+
+    # if print out the running time
+    if verbose:
+        print("Running time of merge_sort: O(n log n)")
+
+    # merge the sorted left and right parts
+    return merge(left, right)
 
 
 def heap_sort():
