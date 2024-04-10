3bce704c56b660560069e9abeab593a1460b92b6
xyluo <36498464+Xiangyongluo@users.noreply.github.com>
2024-04-10 01:40:51 -0700

    format code

--- pyufunc/util_ai/__init__.py
+++ pyufunc/util_ai/__init__.py
@@ -6,13 +6,13 @@
 ##############################################################
 
 from ._error_measurement import (mean_absolute_error,
-                             mean_squared_error,
-                             root_mean_squared_error,
-                             mean_squared_log_error,
-                             mean_absolute_percentage_error,
-                             mean_percentage_error,
-                             r2_score,
-                             )
+                                 mean_squared_error,
+                                 root_mean_squared_error,
+                                 mean_squared_log_error,
+                                 mean_absolute_percentage_error,
+                                 mean_percentage_error,
+                                 r2_score,
+                                 )
 
 __all__ = [
     # error measure
