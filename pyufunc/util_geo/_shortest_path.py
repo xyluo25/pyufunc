c3c742d871b37633d200013d137f31cbe8287e21
xyluo25 <luoxiangyong01@gmal.com>
2024-04-12 14:13:45 -0700

    add message for shortest path

---
+++ pyufunc/util_geo/_shortest_path.py
@@ -0,0 +1,56 @@
+# -*- coding:utf-8 -*-
+##############################################################
+# Created Date: Friday, April 12th 2024
+# Contact Info: luoxiangyong01@gmail.com
+# Author/Copyright: Mr. Xiangyong Luo
+##############################################################
+
+# _shortest_path.py
+# from ._dijkstra import dijkstra
+# from ._floyd import floyd
+# from ._bellman_ford import bellman_ford
+# from ._johnson import johnson
+# from ._astar import astar
+# fomr ._bidirectional_astar import bidirectional_astar
+# from ._landmark import landmark
+# from ._ch import ch
+# from ._contraction import contraction
+# from ._contraction_hierarchical import contraction_hierarchical
+# from ._contraction_parallel import contraction_parallel
+# from ._contraction_parallel_hierarchical import contraction_parallel_hierarchical
+
+# Generally, there are tow type of shortest path algorithms:
+# 1. Single-source shortest path algorithms
+#    - Dijkstra's algorithm
+#    - Bellman-Ford algorithm
+#    - A* search algorithm
+#    - Bidirectional A* search algorithm
+#    - Landmark path algorithm
+#    - Contraction path algorithm
+#    - Contraction Hierarchical path algorithm
+#    - Contraction Parallel path algorithm
+#    - Contraction Parallel Hierarchical path algorithm
+# 2. All-pairs shortest path algorithms
+#    - Floyd-Warshall algorithm
+#    - Johnson's algorithm
+#    - CH algorithm
+
+# Besides, there are two types of implementations for shortest path algorithms:
+# 1. Label setting algorithms
+#    - Dijkstra's algorithm
+#    - Bellman-Ford algorithm
+#    - A* search algorithm
+#    - Bidirectional A* search algorithm
+#    - Landmark path algorithm
+#    - Contraction path algorithm
+#    - Contraction Hierarchical path algorithm
+#    - Contraction Parallel path algorithm
+#    - Contraction Parallel Hierarchical path algorithm
+# 2. Label correcting algorithms
+#    - Floyd-Warshall algorithm
+#    - Johnson's algorithm
+#    - CH algorithm
+#    - Contraction path algorithm
+#    - Contraction Hierarchical path algorithm
+#    - Contraction Parallel path algorithm
+#    - Contraction Parallel Hierarchical path algorithm
