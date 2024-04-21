05a76d3d55f0321deb395d7219ea215e72a09aba
xyluo25 <luoxiangyong01@gmal.com>
2024-04-21 10:54:13 -0700

    add end_of_life decorator

--- pyufunc/pkg_utils.py
+++ pyufunc/pkg_utils.py
@@ -25,6 +25,7 @@ __all__ = [
     "func_running_time",
     "func_time",
     "run_parallel",
+    "end_of_life",
     "get_user_defined_func",
     "is_user_defined_func",
     "is_module_importable",
@@ -188,8 +189,7 @@ def func_running_time(func: object) -> object:
         res = func(*args, **kwargs)
         time_diff = datetime.datetime.now() - time_start
         print(
-            f'  :INFO: finished function: {func.__name__}, total: {time_diff.seconds}s')
-        print()
+            f'  :INFO: finished function: {func.__name__}, total: {time_diff.seconds}s \n')
         return res
 
     return inner
@@ -232,8 +232,7 @@ def func_time(func: object) -> object:
         res = func(*args, **kwargs)
         time_diff = datetime.datetime.now() - time_start
         print(
-            f'  :INFO: finished function: {func.__name__}, total: {time_diff.seconds}s')
-        print()
+            f'  :INFO: finished function: {func.__name__}, total: {time_diff.seconds}s \n')
         return res
 
     return inner
@@ -529,3 +528,56 @@ def run_parallel(func: Callable, iterable: Iterable,
             results = pool.map(func, iterable, chunksize=chunksize)
 
     return results
+
+
+def end_of_life(func: object = None, **kwargs) -> object:
+    """A decorator to mark the end of life of a function or class method.
+    It's useful to use this function to remind users to avoid using the deprecated functions.
+
+    Args:
+        *args: the required dependencies to run the function.
+            if argument is a string, module and import are the same.
+            if argument is a tuple or list, it has two elements:
+                first element is the module name, for pip or conda installation;
+                second element is the import name, for import the module;
+        **kwargs: the optional arguments, including message.
+            message (str, optional): the additional message to the users.
+
+    Returns:
+        object: the decorated function.
+
+    Examples:
+        >>> from pyufunc import end_of_life
+        >>> @end_of_life
+            def my_func():
+                return "I'm running!"
+        >>> my_func()
+          :Warning: my_func is deprecated and will be removed in the future.
+        I'm running!
+
+        >>>@end_of_life(message="Please use the new function instead.")
+           def my_func():
+              return "I'm running!"
+        >>> my_func()
+            :Warning: my_func is deprecated and will be removed in the future.
+            :Please use the new function instead.
+        I'm running!
+    """
+
+    # kwargs from decorator
+    # print("kwargs: ", kwargs)
+    message = kwargs.get("message", None)
+
+    def inner(func_obj: object) -> object:
+        @wraps(func_obj)
+        def wrapper(*args, **kwargs):
+            # kwargs from function
+            print(f"  :Warning: {func_obj.__name__} is deprecated and will be removed in the future version.")
+
+            if message:
+                print(f"  :{message}")
+
+            return func_obj(*args, **kwargs)
+        return wrapper
+
+    return inner(func) if func else inner
