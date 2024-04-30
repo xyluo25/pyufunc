b38e4fadf3f49c474c7d58d59c6126ef62dbfd2e
xyluo25 <luoxiangyong01@gmal.com>
2024-04-30 14:12:27 -0700

    update split_dict_by_chunk

--- pyufunc/util_data_processing/_dict.py
+++ pyufunc/util_data_processing/_dict.py
@@ -6,37 +6,31 @@
 ##############################################################
 
 import itertools
-from typing import Generator
 
 
-def split_dict_by_chunk(dictionary: dict, chunk_size: int, pair_val: list = []) -> Generator:
-    """Split dictionary into chunks
+def split_dict_by_chunk(dictionary: dict, chunk_size: int) -> list:
+    """Split dictionary into a list of chunks
 
     Args:
         dictionary (dict): the input dictionary with key-value pairs
         chunk_size (int): the size of each chunk
-        pair_val (list, optional): the return value associate with each chunk dictionary. Defaults to [].
 
     Returns:
-        Generator: a generator of the list including a chunk value and the pair value: [chunk_dict, pair_val]
-
-    Yields:
-        Iterator[list]: a generator of the list including a chunk value and the pair value: [chunk_dict, pair_val]
+        list: a list of the chunk_dict
 
     Examples:
         >>> import pyufunc as uf
         >>> d = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
         >>> chunk_size = 2
-        >>> pair_val = []
-        >>> res = uf.split_dict_by_chunk(d, chunk_size, pair_val)
-        >>> for r in res:
-        ...     print(r)
-        [{'a': 1, 'b': 2}]
-        [{'c': 3, 'd': 4}]
+        >>> res = uf.split_dict_by_chunk(d, chunk_size)
+        >>> res
+        [{'a': 1, 'b': 2}, {'c': 3, 'd': 4}]
 
     """
+
+    chunk_lst = []
     iterator = iter(dictionary.items())
     for _ in range(0, len(dictionary), chunk_size):
-        chunk = dict(itertools.islice(iterator, chunk_size))
-        if chunk:  # check if chunk is not empty
-            yield [chunk] + pair_val
+        if chunk := dict(itertools.islice(iterator, chunk_size)):
+            chunk_lst.append(chunk)
+    return chunk_lst
