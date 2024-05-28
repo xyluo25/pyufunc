9e5793d6e2c9cf3f61d86ca449e7189a58943864
xyluo25 <luoxiangyong01@gmal.com>
2024-05-27 18:24:10 -0700

    fix errors

--- pyufunc/pkg_configs.py
+++ pyufunc/pkg_configs.py
@@ -104,6 +104,32 @@ config_logging = {
 
     # default log date format
     "log_datefmt": "%Y-%m-%d %H:%M:%S",
+
+    # default log attributes
+    "log_attrs": (
+        "args",
+        "asctime",
+        "created",
+        "exc_info",
+        "exc_text",
+        "filename",
+        "funcName",
+        "levelname",
+        "levelno",
+        "lineno",
+        "module",
+        "msecs",
+        "message",
+        "msg",
+        "name",
+        "pathname",
+        "process",
+        "processName",
+        "relativeCreated",
+        "stack_info",
+        "thread",
+        "threadName"),
+
 }
 
 # ############### Date Time Format Configuration ############### #
